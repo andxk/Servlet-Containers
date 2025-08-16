e4233b7fc7f6d5a560cbbf7ea22ced421870ae94
andxk <kupex@ya.ru>
2025-08-16 03:39:08 +0300

    Pom from demo, add gson dependency

--- src/main/java/ru/netology/initializer/ApplicationInitializer.java
+++ src/main/java/ru/netology/initializer/ApplicationInitializer.java
@@ -1,39 +1,11 @@
-/*
-package ru.netology.initializer;
-
-import jakarta.servlet.Servlet;
-import jakarta.servlet.ServletException;
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
-
-//import jakarta.servlet.ServletContext;
-import javax.servlet.ServletContext;
-
-public class ApplicationInitializer implements WebApplicationInitializer {
-
-    @Override
-    public void onStartup(ServletContext servletContext) {
-        final var context = new AnnotationConfigWebApplicationContext();
-        context.scan("ru.netology");
-        context.refresh();
-
-        final var servlet = new DispatcherServlet(context);
-        final var registration = servletContext.addServlet("app", servlet);
-        registration.setLoadOnStartup(1);
-        registration.addMapping("/");
-    }
-
-}
-*/
-
 package ru.netology.initializer;
 
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
-import javax.servlet.ServletContext;
+//import javax.servlet.ServletContext;
+import jakarta.servlet.ServletContext;
 
 public class ApplicationInitializer implements WebApplicationInitializer {
     @Override
